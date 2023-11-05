97753ee7d36fdca802128c6c61bd6a57109aec7d
MJKim <qorhaqt@gmail.com>
2023-11-05 23:08:26 +0900

    Add: UIKit을 스토리보드에서 코드 베이스로 넘어가기 위해 학습중,,,생각보다 어렵다

---
+++ CodeBaseAutolayout/CodeBaseAutolayout/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableView.swift
+//  CodeBaseAutolayout
+//
+//  Created by Minjae Kim on 11/5/23.
+//
+
+import UIKit
+
+class TableView: UITableViewCell {
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		self.contentView.backgroundColor = .white
+		
+	}
+}
