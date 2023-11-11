00d298c46d1effdc4ead63bb862c856224a4f458
MJKim <qorhaqt@gmail.com>
2023-11-11 23:43:32 +0900

    Add: Activity Indicator 컴포넌트 학습

---
+++ CodeBaseAutolayout/CodeBaseAutolayout/ActivityIndicatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityIndicatorViewController.swift
+//  CodeBaseAutolayout
+//
+//  Created by Minjae Kim on 11/11/23.
+//
+
+import UIKit
+
+class ActivityIndicatorViewController: UIViewController {
+	let activityIndicator: UIActivityIndicatorView = {
+		let view: UIActivityIndicatorView = UIActivityIndicatorView(style: .medium)
+		view.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.startAnimating()
+		return view
+	}()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .systemBackground
+		setView()
+	}
+	private func setView() {
+		self.view.addSubview(activityIndicator)
+		NSLayoutConstraint.activate([
+			self.activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+			self.activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+		])
+	}
+}
+
+#Preview {
+	ActivityIndicatorViewController()
+}
