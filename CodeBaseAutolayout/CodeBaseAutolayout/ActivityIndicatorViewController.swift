09b310ba516af47bc7320eaf9ed28edbc779de78
MJKim <qorhaqt@gmail.com>
2023-11-12 20:37:26 +0900

    [Modify] 주석 코드 삭제 및 설명 주석 추가

--- CodeBaseAutolayout/CodeBaseAutolayout/ActivityIndicatorViewController.swift
+++ CodeBaseAutolayout/CodeBaseAutolayout/ActivityIndicatorViewController.swift
@@ -12,6 +12,8 @@ class ActivityIndicatorViewController: UIViewController {
 		let view: UIActivityIndicatorView = UIActivityIndicatorView(style: .medium)
 		view.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
 		view.translatesAutoresizingMaskIntoConstraints = false
+		
+		// 이 코드를 줘야 돌아가는 애니메이션 작동
 		view.startAnimating()
 		return view
 	}()
